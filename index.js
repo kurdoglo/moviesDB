cf4b83faeab288267e3c8385b4ed2f3ef50e4766
kurdoglo <vitalik8427@yandex.ru>
2023-01-04 19:21:59 +0200

    Loop the questions until 2

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ let personalMovieDB = {
   genres: [],
   private: false,
 };
-const a = prompt("Один из последних просмотренных фильмов?"),
-  b = prompt("На сколько оцените его?"),
-  c = prompt("Один из последних просмотренных фильмов?"),
-  d = prompt("На сколько оцените его?");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?");
+  personalMovieDB.movies[a] = b;
+}
 console.log(personalMovieDB);
