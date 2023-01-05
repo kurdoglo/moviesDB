87cc1ef30db8da8391e484c396d50d23eb0c93a3
kurdoglo <vitalik8427@yandex.ru>
2023-01-05 17:14:48 +0200

    Add two more types of loop declaration (while and do...while)

--- index.js
+++ index.js
@@ -7,10 +7,54 @@ let personalMovieDB = {
   genres: [],
   private: false,
 };
+// const a = prompt("Один из последних просмотренных фильмов?"),
+//   b = prompt("На сколько оцените его?");
+// personalMovieDB.movies[a] = b;
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?"),
+//     b = prompt("На сколько оцените его?");
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+//   console.log(personalMovieDB);
+// }
+
+// if (personalMovieDB.count <= 10) {
+//   console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log("Вы классический зритель");
+// } else if (personalMovieDB.count > 30) {
+//   console.log("Вы киноман");
+// } else {
+//   console.log("Произошла ошибка");
+// }
+// console.log(personalMovieDB);
+
+// Two more loop methods //
+let i = 0;
+while (i < 2) {
+  i++;
   const a = prompt("Один из последних просмотренных фильмов?"),
     b = prompt("На сколько оцените его?");
   personalMovieDB.movies[a] = b;
 }
+console.log(personalMovieDB);
+
+do {
+  i++;
+  const a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+} while (i < 1);
 console.log(personalMovieDB);
